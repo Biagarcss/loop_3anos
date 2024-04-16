480f9ea840b8edbeb1770429a77206c47f9c0eeb
Bianca Garcia <106812838+Biagarcss@users.noreply.github.com>
2024-04-16 12:03:52 -0300

    teste

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function verificarTime() {
     var respostaTime = prompt("Qual a área da saúde que se utiliza laboratório?");
 
     
-    if (respostaTime.toLowerCase() === "Biomedicina") {
+    if (respostaTime.toLowerCase() === "biomedicina") {
       alert("Isso mesmo! È na Biomedicina que se utiliza laboratório para exames e analises!");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
